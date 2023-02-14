c4c76d3dd55775db0f61c4f11b04d40c05554343
goutarouh <gouta.gorogoro@gmail.com>
2023-02-14 23:05:53 +0900

    second commit
    - schemeの追加
    - queryの追加
    - permissionの追加
    - 試し呼び出しの追加

--- app/src/main/java/com/github/goutarouh/graphqlpokemonpaging/MainActivity.kt
+++ app/src/main/java/com/github/goutarouh/graphqlpokemonpaging/MainActivity.kt
@@ -2,10 +2,25 @@ package com.github.goutarouh.graphqlpokemonpaging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.apollographql.apollo3.ApolloClient
+import com.example.GetPokemonQuery
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create a client
+        val apolloClient = ApolloClient.Builder()
+            .serverUrl("https://graphql-pokemon2.vercel.app")
+            .build()
+
+        // Execute your query. This will suspend until the response is received.
+        runBlocking {
+            val response = apolloClient.query(GetPokemonQuery()).execute()
+            findViewById<TextView>(R.id.text_text).text = response.data?.pokemon?.name
+        }
     }
 }
